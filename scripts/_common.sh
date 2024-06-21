609d2c4468bf26204e4bf64fb16f715929a75afa
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-06-21 09:52:43 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
 #=================================================
-# COMMON VARIABLES
-#=================================================
-
-#=================================================
-# PERSONAL HELPERS
+# COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
 configure_element()
 {
-    ynh_add_config --template="config.json" --destination="$install_dir/config.json"
+    ynh_config_add --template="config.json" --destination="$install_dir/config.json"
     chmod -R u=rwX,g=rX,o= "$install_dir"
     chown -R $app:www-data "$install_dir"
 }
-
-#=================================================
-# EXPERIMENTAL HELPERS
-#=================================================
-
-#=================================================
-# FUTURE OFFICIAL HELPERS
-#=================================================
